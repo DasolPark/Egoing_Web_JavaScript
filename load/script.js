6261c100498908f948b5f92330f309666c16328f
Dasol Park <dasol.park615@gmail.com>
2019-03-15 21:50:48 +0800

    Load JavaScript from HTML - The location of the script file

--- load/script.js
+++ load/script.js
@@ -1,4 +1,15 @@
-const hw = document.getElementById("hw");
-hw.addEventListener("click", () => {
-  alert("Hello World!");
-});
+window.onload = () => {
+  const hw = document.getElementById("hw");
+  hw.addEventListener("click", () => {
+    alert("Hello World!");
+  });
+};
+
+// Old Version
+
+// window.onload = function() {
+//   var hw = document.getElementById("hw");
+//   hw.addEventListener("click", function() {
+//     alert("Hello World!");
+//   });
+// };
