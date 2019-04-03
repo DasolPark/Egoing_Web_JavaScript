addb6ab48bf3ae21a85e05e2831d7b4c33adf871
Dasol Park <dasol.park615@gmail.com>
2019-04-03 23:06:02 +0800

    Network Communication - JSON(JSON API, AJAX&JSON)

---
+++ load/JSONIntro.js
@@ -0,0 +1,15 @@
+// JSON
+var person = { height: 180, job: "programmer" };
+// Array
+var members = ["dasol", "hyunjin", "love ing"];
+
+var memberList = [
+  {
+    name: "Dasol",
+    age: "29"
+  },
+  {
+    name: "Judy",
+    age: "23"
+  }
+];
