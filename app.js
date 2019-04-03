4eedc77634451c71237a575c087a63e5df03d88a
Dasol Park <dasol.park615@gmail.com>
2019-04-03 21:57:04 +0800

    Network Communication - AJAX(GET, POST)

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/load/interaction.html");
 });
 
+app.get("/bio", function(req, res) {
+  res.sendFile(__dirname + "/load/netComAJAXGet.html");
+});
+
+app.get("/bioText", function(req, res) {
+  res.sendFile(__dirname + "/load/Bio.txt");
+});
+
+app.get("/Layer.png", function(req, res) {
+  res.sendFile(__dirname + "/load/Layer.png");
+});
+
 app.listen("8888", function() {
   console.log("port 8888!");
 });
